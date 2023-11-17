d29a6be08f83b7b6c0a4c662959b522fdc166610
Jacey Davis <jaceyd923@gmail.com>
2023-11-16 20:27:30 -0700

    user can choose a pet

--- client/main.js
+++ client/main.js
@@ -1,6 +1,15 @@
 const dashboard = document.querySelector('#dashboard')
+const choosePet = document.querySelector('#choose-pet')
+const petPic = document.querySelector('#pet-pic')
 
-const toggleDashboard = () => {
-    dashboard.classList.toggle('hidden')
+for (const child of choosePet.children) {
+    child.addEventListener('click', () => renderGame(child.id))
 }
 
+const renderGame = (pet) => {
+    petPic.src = `images/${pet}.jpg`
+    dashboard.classList.remove('hidden')
+
+    const beginScreen = document.querySelector('#begin-screen')
+    beginScreen.classList.add('hidden')
+}
