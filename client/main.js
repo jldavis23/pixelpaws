16fd385794c2c7fe9b5ea1ae5646cf0aee2222ef
Jacey Davis <jaceyd923@gmail.com>
2023-11-22 13:24:59 -0700

    user can buy items

--- client/main.js
+++ client/main.js
@@ -4,6 +4,8 @@ const choosePetForm = document.querySelector('#choose-pet-form')
 const dashboard = document.querySelector('#dashboard')
 dashboard.remove()
 
+//DATA
+let money = 80
 
 let myPet = {
     type: '',
@@ -11,15 +13,24 @@ let myPet = {
     personality: '',
 }
 
+let myInventory = {
+    apple: 0,
+    kibble: 0,
+    treat: 0,
+    medicine: 0
+}
+
 const renderGame = (e) => {
     e.preventDefault()
 
+    // Add the dashboard to the DOM
     main.appendChild(dashboard)
 
-    decayPetNeeds('happiness')
-    decayPetNeeds('health')
-    decayPetNeeds('hunger')
-    decayPetNeeds('sleep')
+    // Start decaying the pet's needs for each category
+    decayPetNeed('happiness')
+    decayPetNeed('health')
+    decayPetNeed('hunger')
+    decayPetNeed('sleep')
 
     // Assign the selected pet type
     const petOptions = document.getElementsByName('pet-options')
@@ -30,7 +41,7 @@ const renderGame = (e) => {
     myPet.type = selectedPet
     const petPic = document.querySelector('#pet-pic')
     petPic.src = `images/${selectedPet}.jpg`
-    
+
     // Assign the inputed name
     const nameInput = document.querySelector('#name-input')
     myPet.name = nameInput.value
@@ -46,15 +57,36 @@ const renderGame = (e) => {
     myPet.personality = selectedPersonality
     const petPersonality = document.querySelector('#pet-personality')
     petPersonality.textContent = selectedPersonality
-    
+
+    // Display the player's money
+    const moneyDisplay = document.querySelector('#money')
+    moneyDisplay.textContent = money
+
+    // Remove the beginning Choose Pet Form
     choosePetForm.remove()
+
+
+
+    const purchaseItemsModal = document.querySelector('#purchase-items-modal')
+    const purchaseItemsBtn = document.querySelector('#purchase-items-btn')
+    purchaseItemsBtn.addEventListener('click', () => purchaseItemsModal.showModal())
+
+    document.querySelector('#purchase-items-money').textContent = money
+
+    const buyBtns = document.querySelectorAll('.buy-btns')
+    buyBtns.forEach(btn => {
+        btn.addEventListener('click', () => purchaseAnItem(btn.name, parseInt(btn.value)))
+    })
+
+
+
 }
 
 const beginGameBtn = document.querySelector('#begin-game')
 beginGameBtn.addEventListener('click', renderGame)
 
 // PET NEEDS DECAY ---------------------------------------------
-const decayPetNeeds = (need) => {
+const decayPetNeed = (need) => {
     const needBar = document.querySelector(`#${need}-bar`)
     const needLabel = document.querySelector(`#${need}-label`)
 
@@ -62,11 +94,30 @@ const decayPetNeeds = (need) => {
         setTimeout(() => {
             needBar.value = needBar.value - 1
             needLabel.textContent = `${needBar.value}%`
-            decayPetNeeds(need)
+            decayPetNeed(need)
         }, 5000)
-    }   
+    }
 }
 
+
+
+
+
+const purchaseAnItem = (item, price) => {
+    if (money >= price) {
+        // Subtract price from Money, display in the DOM
+        money = money - price
+        document.querySelector('#money').textContent = money //main money display
+        document.querySelector('#purchase-items-money').textContent = money //money display inside modal
+
+        // Add item to inventory object
+        myInventory[item]++
+    } else {
+        console.log('not enough money')
+    }
+
+
+}
 
 
 // for (const child of choosePet.children) {
